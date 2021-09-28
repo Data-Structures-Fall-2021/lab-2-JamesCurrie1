43499507842bdaae4468f6474e63e2f0c6da2262
JamesCurrie1 <90404815+JamesCurrie1@users.noreply.github.com>
2021-09-28 12:52:59 -0400

    Update Song.java

--- src/main/java/Song.java
+++ src/main/java/Song.java
@@ -1,5 +1,42 @@
 public class Song {
+    private String artist;
+    private String title;
+    private int duration;
+    public static String collectionName;
+    public Song(String artist, String title, int duration) {
+        this.artist = artist;
+        this.title = title;
+        this.duration = duration;
+    }
 
-	// YOUR CODE GOES HERE
-	
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String toString() {
+        return title + " ("+ artist + ") - "+ (duration - duration%60)/60 + ":"+ (duration%60);
+
+    }
 }
